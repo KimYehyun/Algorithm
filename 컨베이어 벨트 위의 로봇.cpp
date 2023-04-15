de3591b2dd08e3ad1978e190b28778c5e650825d
KimYehyun <77717564+KimYehyun@users.noreply.github.com>
2023-04-16 02:21:54 +0900

    Add files via upload

--- 컨베이어 벨트 위의 로봇.cpp
+++ 컨베이어 벨트 위의 로봇.cpp
@@ -3,15 +3,34 @@
 using namespace std;
 
 int n, k;
-deque<int> belt(201);
+deque<int> belt;
 int cnt;
-bool visit[101];
+deque<bool> visit;
 
 void move_belt() {
-	belt.push_front(belt[2*n-1]);
-	belt.erase(belt.begin() + (2*n));
+
+	belt.push_front(belt.back());
+	belt.pop_back();
+	
+	visit.push_front(visit.back());
+	visit.pop_back();
+	visit[n - 1] = false;
+	
 	return;
 }
+void move_robot() {
+
+	for (int i = n-2; i >= 0; i--) {
+		if ((visit[i + 1] == false && belt[i+1]>0)&&visit[i]==true) {
+			visit[i] = false;
+			visit[i + 1] = true;
+			belt[i+1]--;
+		}
+		visit[n - 1] = false;
+	}
+	return;	
+}
+
 void new_robot() {
 	if (visit[0] == false && belt[0] >= 1) {
 		visit[0] = true;
@@ -20,21 +39,39 @@ void new_robot() {
 	return;
 }
 
-int main(void) {
-
-	cin >> n >> k;
-	for (int i = 0; i < 2*n; i++) {
-		cin >> belt[i];
+int check() {
+	int count = 0;
+	for (int i = 0; i < n * 2; i++) {
+		if (belt[i] == 0)
+			count++;
 	}
-
-	while (cnt < k) {
-		cnt++;
-		move_belt();
-		new_robot();
-
-	}
-	cout << cnt << endl;
-	
-	return 0;
+	return count;
 }
 
+
+int main(void) {
+	cnt = 1;
+	cin >> n >> k;
+	for (int i = 0; i < 2 * n; i++) {
+		int a;
+		cin >> a;
+		belt.push_back(a);
+		visit.push_back(false);
+	}
+	
+	while (1) {
+	
+		move_belt();
+		move_robot();
+		new_robot();
+		int c = check();
+		if (c >= k) {
+			cout << cnt << endl;
+			return 0;
+		}
+		cnt++;	
+	}
+
+
+	return 0;
+}
