ca399cd6eb347b2fdecc430e7886c13f09cc9ec1
KimYehyun <77717564+KimYehyun@users.noreply.github.com>
2023-04-18 15:57:37 +0900

    Add files via upload

--- 마법사 상어와 비바라기.cpp
+++ 마법사 상어와 비바라기.cpp
@@ -1,14 +1,24 @@
 #include<iostream>
 #include<vector>
 using namespace std;
-int n, m;
+int n, m, answer;
 int A[51][51];
-vector<pair<int, int>>v;
+vector<pair<int, int>>cloud;
 
+int dx[] = { 0,-1,-1,0,1,1,1,0, -1 };
+int dy[] = { 0,0,1,1,1,0,-1,-1, -1 };
 
-void move() {
-							
+void move(int d, int s) {
+	s = s % n;
+	for (int i = 1; i <= cloud.size(); i++)
+	{
+		cloud[i].first = (cloud[i].first + s * dx[d] + n) % n;
+		cloud[i].second = (cloud[i].second + s * dy[d] + n) % n;
+		A[cloud[i].first][cloud[i].second]++; 
+	}
+	return;
 }
+
 
 int main(void) {
 	
@@ -18,16 +28,24 @@ int main(void) {
 			scanf("%d", &A[i][j]);
 		}
 	}
-	for (int i = 1; i <= m; i++) {
-		int a, b;
-		scanf("%d %d", &a, &b);
-		v.push_back(make_pair(a, b));
-	}
+
+	cloud.push_back(make_pair(n, 1));
+	cloud.push_back(make_pair(n, 2));
+	cloud.push_back(make_pair(n - 1, 1));
+	cloud.push_back(make_pair(n - 1, 2));
 
 	for (int i = 1; i <= m; i++) {
-		move();
+		int d, s;
+		scanf("%d %d", &d, &s);
+		move(d,s); //order의 i번째 입력에 따라 구름 이동	
 	}
-	
+
+	for (int i = 1; i <= n; i++) {
+		for (int j = 1; j <= n; j++) {
+			printf("%d ", A[i][j]);
+		}
+		printf("\n");
+	}
 
 
 	return 0;
