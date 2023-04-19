63b4568510084c6a16daef4491446cd1875d5a50
KimYehyun <77717564+KimYehyun@users.noreply.github.com>
2023-04-20 02:33:03 +0900

    Add files via upload

--- 마법사 상어와 비바라기.cpp
+++ 마법사 상어와 비바라기.cpp
@@ -6,21 +6,21 @@ int A[51][51];
 vector<pair<int, int>>cloud;
 bool cloud_visit[51][51];
 
-int dx[9] = { 0,0,1,1,1,0,-1,-1,-1 };
-int dy[9] = { 0,-1,-1,0,1,1,1,0,-1 };
+int dx[9] = { 0, 0, -1, -1, -1, 0, 1, 1, 1 };
+int dy[9] = { 0, -1, -1, 0, 1, 1, 1, 0, -1 }; 
 
 void move_cloud(int d, int s) {
 
 	for (int i = 0; i < cloud.size(); i++) {
 		int nx = cloud[i].first + dx[d] * s;
 		int ny = cloud[i].second + dy[d] * s;
-		while (nx < 0) {
+		while (nx <= 0) {
 			nx += n;
 		}
 		while (nx > n) {
 			nx -= n;
 		}
-		while (ny < 0) {
+		while (ny <= 0) {
 			ny += n;
 		}
 		while (ny > n) {
@@ -29,28 +29,10 @@ void move_cloud(int d, int s) {
 		cloud[i].first = nx;
 		cloud[i].second = ny;
 
-		printf("(nx=%d,ny=%d)\n", nx, ny);
-
 		A[cloud[i].first][cloud[i].second]++;
 		cloud_visit[cloud[i].first][cloud[i].second] = true;
 	}
-	printf("\n");
-	printf("구름이동\n");
-	for (int i = 1; i <= n; i++) {
-		for (int j = 1; j <= n; j++) {
-			printf("%d ", A[i][j]);
-		}
-		printf("\n");
-	}
-	printf("\n");
-	printf("구름 위치 저장 bool값\n");
-	for (int i = 1; i <= n; i++) {
-		for (int j = 1; j <= n; j++) {
-			printf("%d ", cloud_visit[i][j]);
-		}
-		printf("\n");
-	}
-	
+
 	return;
 }
 
@@ -65,14 +47,6 @@ void water_copy() {
 		}
 		A[cloud[i].first][cloud[i].second] += water;
 		water = 0;
-	}
-	printf("\n");
-	printf("물 복사\n");
-	for (int i = 1; i <= n; i++) {
-		for (int j = 1; j <= n; j++) {
-			printf("%d ", A[i][j]);
-		}
-		printf("\n");
 	}
 	return;
 }
@@ -92,26 +66,7 @@ void make_cloud() {
 			cloud_visit[i][j] = false;
 		}
 	}
-	printf("\n");
-	printf("구름 좌표\n");
-	for (int i = 0; i < cloud.size(); i++) {
-		printf("x=%d y=%d\n", cloud[i].first, cloud[i].second);
-	}
-	printf("\n");
-	printf("구름 생성\n");
-	for (int i = 1; i <= n; i++) {
-		for (int j = 1; j <= n; j++) {
-			printf("%d ", A[i][j]);
-		}
-		printf("\n");
-	}
-	printf("구름 위치 삭제 bool값\n");
-	for (int i = 1; i <= n; i++) {
-		for (int j = 1; j <= n; j++) {
-			printf("%d ", cloud_visit[i][j]);
-		}
-		printf("\n");
-	}
+	return;
 }
 
 int sum_water() {
