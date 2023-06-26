dd966f0cc32ff999c790042627289f798501701e
KimYehyun <77717564+KimYehyun@users.noreply.github.com>
2023-06-26 20:20:19 +0900

    [Gold IV] Title: 최단경로, Time: 148 ms, Memory: 9072 KB -BaekjoonHub

---
+++ 백준/Gold/1753. 최단경로/최단경로.cc
@@ -0,0 +1,69 @@
+#include<iostream>
+#include<queue>
+#include<vector>
+
+#define MAX 20001
+#define INF 987654321
+using namespace std;
+
+int V, E, Start;
+int dist[MAX];
+vector<pair<int, int>> graph[MAX];
+
+void dijkstra()
+{
+    priority_queue<pair<int, int>> pq;
+    pq.push(make_pair(0, Start));
+    dist[Start] = 0;
+
+    while (!pq.empty())
+    {
+        int cost = -pq.top().first;
+        int cur = pq.top().second;
+        pq.pop();
+
+        if (dist[cur] < cost) continue;
+
+        for (int i = 0; i < graph[cur].size(); i++)
+        {
+            int next = graph[cur][i].first;
+            int nCost = graph[cur][i].second;
+
+            if (dist[next] > cost + nCost)
+            {
+                dist[next] = cost + nCost;
+                pq.push(make_pair(-dist[next], next));
+            }
+        }
+    }
+
+   
+}
+
+
+int main(void)
+{
+    scanf("%d %d", &V, &E);
+    scanf("%d", &Start);
+    for (int i = 0; i < E; i++)
+    {
+        int u, v, w; 
+        scanf("%d %d %d", &u, &v, &w);
+        graph[u].push_back(make_pair(v, w));
+    }
+
+    for (int i = 1; i <= V; i++) 
+        dist[i] = INF;
+
+    dijkstra();
+
+    for (int i = 1; i <= V; i++)
+    {
+        if (dist[i] == INF)
+            printf("INF\n");
+        else
+            printf("%d\n", dist[i]);
+    }
+
+    return 0;
+}
