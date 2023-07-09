3f9cef16e4f90cb3092ca507448fbe446ac5dde4
KimYehyun <77717564+KimYehyun@users.noreply.github.com>
2023-07-09 22:30:59 +0900

    [Bronze V] Title: 사칙연산, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10869. 사칙연산/사칙연산.cc
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+
+int main(void) {
+
+	int n,m;
+	scanf("%d %d",&n,&m);
+
+	printf("%d\n", n + m);
+	printf("%d\n", n - m);
+	printf("%d\n", n * m);
+	printf("%d\n", n / m);
+	printf("%d\n", n % m);
+	return 0;
+}
