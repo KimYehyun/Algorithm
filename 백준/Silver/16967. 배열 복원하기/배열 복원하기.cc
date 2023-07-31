f4204583c6f49442f3896e4294de99dcae5e4158
KimYehyun <77717564+KimYehyun@users.noreply.github.com>
2023-07-31 23:02:25 +0900

    [Silver III] Title: 배열 복원하기, Time: 84 ms, Memory: 3432 KB -BaekjoonHub

---
+++ 백준/Silver/16967. 배열 복원하기/배열 복원하기.cc
@@ -0,0 +1,28 @@
+#include <iostream>
+
+using namespace std;
+
+int h,w, x, y;
+int arr[601][601];
+int main() {
+
+	cin >> h >> w >> x >> y;
+	for (int i = 0; i < h + x; i++) {
+		for (int j = 0; j < w + y; j++) {
+			cin >> arr[i][j];
+		}
+	}
+	for (int i = x; i < h; i++) {
+		for (int j = y; j < w; j++) {
+			arr[i][j] -= arr[i - x][j - y];
+		}
+	}
+	for (int i = 0; i < h; i++) {
+		for (int j = 0; j < w; j++) {
+			cout << arr[i][j] << " ";
+		}
+		cout << "\n";
+	}
+
+	return 0;
+}
