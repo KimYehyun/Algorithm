e630f33a68fb4e5423b979480301e887c2c4efca
김예현 <77717564+KimYehyun@users.noreply.github.com>
2024-01-11 18:22:45 +0900

    [Silver IV] Title: 팰린드롬 애너그램, Time: 8 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Silver/30458. 팰린드롬 애너그램/팰린드롬 애너그램.cc
+++ 백준/Silver/30458. 팰린드롬 애너그램/팰린드롬 애너그램.cc
@@ -1,34 +1,26 @@
 #include<iostream>
 #include<map>
 using namespace std;
-
 int main(void) {
-    ios_base::sync_with_stdio(false);
-    cin.tie(0); cout.tie(0);
-
-    map<char, int> m;
-    int n;
-    cin >> n;
-    string s;
-    cin >> s;
-
-    for (int i = 0; i < n; i++) {
-        if (n % 2 == 1 && i == n / 2) continue;
-        m[s[i]]++;
-    }
-
-    bool a = 0;
-    for (auto k : m) {
-        if (k.second % 2 != 0) {
-            if (a) {
-                cout << "No";
-                return 0;
-            }
-            a = 1;
-        }
-    }
-
-    cout << "Yes";
-
-    return 0;
+	ios_base::sync_with_stdio(false);
+	cin.tie(0); cout.tie(0);
+	map<char, int>m;
+	int n;
+	cin >> n;
+	for (int i = 0; i < n; i++) {
+        char a;
+		cin >> a;
+		if (n % 2 == 1 && i == n / 2) continue;
+		m[a]++;
+	}
+	bool b = 0;
+	for (auto k : m) {
+		if (k.second % 2 != 0) {
+			b = 1;
+		}
+	}
+	if (b == 0) cout << "Yes";
+	else cout << "No";
+	
+	return 0;
 }
