b8f775bf2fd7150efdc43420765b05023c6f8ee9
김예현 <77717564+KimYehyun@users.noreply.github.com>
2024-01-16 14:35:55 +0900

    [Silver II] Title: 유기농 배추, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Silver/1012. 유기농 배추/유기농 배추.cc
+++ 백준/Silver/1012. 유기농 배추/유기농 배추.cc
@@ -1,62 +1,54 @@
 #include<iostream>
-#include<string.h>
 #include<queue>
+#include<string.h>
 using namespace std;
-
-int arr[51][51];
-int dx[4] = { 1,0,-1,0 };
-int dy[4] = { 0,-1,0,1 };
-void bfs(int x,int y,int n,int m) {
+bool arr[51][51];
+int t, m, n, k;
+int dx[4] = { 0,1,0,-1 };
+int dy[4] = { 1,0,-1,0 };
+void bfs(int i, int j) {
 	queue<pair<int, int>>q;
-	q.push(make_pair(x, y));
-	arr[x][y] = 0;
-
+	q.push({ i,j });
+	arr[i][j] = 0;
 	while (!q.empty()) {
 		int cur_x = q.front().first;
 		int cur_y = q.front().second;
 		q.pop();
 		for (int i = 0; i < 4; i++) {
-			int next_x = cur_x + dx[i];
-			int next_y = cur_y + dy[i];
-			if (next_x >= 0 && next_x < n && next_y >= 0 && next_y < m && arr[next_x][next_y] == 1) {
-				q.push(make_pair(next_x, next_y));
-				arr[next_x][next_y] = 0;
+			int nx = cur_x + dx[i];
+			int ny = cur_y + dy[i];
+			if (nx >= 0 && nx < n && ny >= 0 && ny < m && arr[nx][ny]!=0) {
+				q.push({ nx,ny });
+				arr[nx][ny] = 0;
 			}
 		}
 	}
-	return;
 }
 
 int main(void) {
-	
-	int T;
-	scanf("%d", &T);
-	for (int i = 0; i < T; i++){
-
-		int n, m, c;
-		scanf("%d %d %d", &n, &m, &c);
-
-		for (int i = 0; i < c; i++) {
+	ios_base::sync_with_stdio(false);
+	cin.tie(0); cout.tie(0);
+	cin >> t;
+	for (int tc = 0; tc < t; tc++) {	
+		cin >> n >> m >> k;
+		memset(arr, 0, sizeof(arr));
+		for (int i = 0; i < k; i++) {
 			int a, b;
-			scanf("%d %d", &a, &b);		
+			cin >> a >> b;
 			arr[a][b] = 1;
 		}
-
-		int cnt = 0;
-
+		int ans = 0;
 		for (int i = 0; i < n; i++) {
 			for (int j = 0; j < m; j++) {
 				if (arr[i][j] == 1) {
-					bfs(i,j,n,m);
-					cnt++;
-				}		
+					bfs(i, j);
+					ans++;
+				}
 			}
 		}
-
-		cout << cnt << endl;
-		for (int i = 0; i < m; i++)
-			memset(arr[i], 0, sizeof(int) * n);
+		cout << ans << "\n";
 
 	}
+
 	return 0;
 }
