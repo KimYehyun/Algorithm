985368adaba1ca2994bc54e78f5b9ffdba7fc951
김예현 <77717564+KimYehyun@users.noreply.github.com>
2024-01-18 23:00:54 +0900

    [Silver II] Title: 유기농 배추, Time: 0 ms, Memory: 2032 KB -BaekjoonHub

--- 백준/Silver/1012. 유기농 배추/유기농 배추.cc
+++ 백준/Silver/1012. 유기농 배추/유기농 배추.cc
@@ -2,36 +2,36 @@
 #include<queue>
 #include<string.h>
 using namespace std;
-bool arr[51][51];
-int t, m, n, k;
-int dx[4] = { 0,1,0,-1 };
-int dy[4] = { 1,0,-1,0 };
-void bfs(int i, int j) {
+int n, m, k;
+int arr[51][51];
+int dx[4] = { 1,0,-1,0 };
+int dy[4] = { 0,1,0,-1 };
+void bfs(int i,int j){
 	queue<pair<int, int>>q;
 	q.push({ i,j });
 	arr[i][j] = 0;
 	while (!q.empty()) {
-		int cur_x = q.front().first;
-		int cur_y = q.front().second;
+		int x = q.front().first;
+		int y = q.front().second;
 		q.pop();
 		for (int i = 0; i < 4; i++) {
-			int nx = cur_x + dx[i];
-			int ny = cur_y + dy[i];
-			if (nx >= 0 && nx < n && ny >= 0 && ny < m && arr[nx][ny]!=0) {
+			int nx = x + dx[i];
+			int ny = y + dy[i];
+			if (nx >= 0 && nx < n && ny >= 0 && ny < m && arr[nx][ny] == 1) {
 				q.push({ nx,ny });
 				arr[nx][ny] = 0;
 			}
 		}
 	}
+	return;
 }
-
 int main(void) {
 	ios_base::sync_with_stdio(false);
 	cin.tie(0); cout.tie(0);
+	int t;
 	cin >> t;
-	for (int tc = 0; tc < t; tc++) {	
+	for (int tc = 0; tc < t; tc++) {
 		cin >> n >> m >> k;
-		memset(arr, 0, sizeof(arr));
 		for (int i = 0; i < k; i++) {
 			int a, b;
 			cin >> a >> b;
@@ -41,14 +41,13 @@ int main(void) {
 		for (int i = 0; i < n; i++) {
 			for (int j = 0; j < m; j++) {
 				if (arr[i][j] == 1) {
-					bfs(i, j);
+					bfs(i,j);
 					ans++;
 				}
 			}
 		}
 		cout << ans << "\n";
-
+		memset(arr, 0, sizeof(arr));
 	}
-
 	return 0;
 }
