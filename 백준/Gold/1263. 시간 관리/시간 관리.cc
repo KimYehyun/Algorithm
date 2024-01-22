471c0890f99d344b00560e5b329b1624d7665cff
김예현 <77717564+KimYehyun@users.noreply.github.com>
2024-01-22 11:08:50 +0900

    [Gold V] Title: 시간 관리, Time: 0 ms, Memory: 2160 KB -BaekjoonHub

--- 백준/Gold/1263. 시간 관리/시간 관리.cc
+++ 백준/Gold/1263. 시간 관리/시간 관리.cc
@@ -4,9 +4,7 @@
 using namespace std;
 int n;
 vector<pair<int,int>>v;
-bool cmp(pair<int, int>& a, pair<int, int>& b) {
-	return a.second > b.second;
-}
+
 int main(void) {
 	ios_base::sync_with_stdio(false);
 	cin.tie(0); cout.tie(0);
@@ -14,25 +12,17 @@ int main(void) {
 	for (int i = 0; i < n; i++) {
 		int a, b;
 		cin >> a >> b;
-		v.push_back({ a,b });
+		v.push_back({ b,a });
 	}
-	sort(v.begin(), v.end(),cmp);
-	int time = 0;
-	int key = 0;
-
-	for (int i = 0; i < n; i++) {
-		if (i != 0) {
-			if (v[i].second >= time) key += v[i].second - time;
-			else if (time - v[i].second >= key) key = 0;
-			else if (time - v[i].second < key) key -= time - v[i].second;
-		}
-		time = v[i].second - v[i].first;
+	sort(v.begin(), v.end(),greater<pair<int,int>>());
+	
+	int time = v[0].first;
+	for (auto a : v) {
+		time = min(a.first, time);
+		time -= a.second;
 	}
-
-	if (key > 0) time -= key;
-
-	if (time < 0) cout << -1 ;
-	else cout << time << endl;
-
+	if (time < 0) cout << -1;
+	else cout << time;
+	
 	return 0;
 }
