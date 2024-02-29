10b57bd7b3817514a21310a864754b6a63b2843c
김예현 <77717564+KimYehyun@users.noreply.github.com>
2024-02-29 10:53:20 +0900

    [Silver V] Title: 최소공배수, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/13241. 최소공배수/최소공배수.cc
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(0); cout.tie(0);
+    long long a,b;
+    cin >> a >> b;
+    cout << lcm(a,b)<<"\n";
+    return 0;
+}
