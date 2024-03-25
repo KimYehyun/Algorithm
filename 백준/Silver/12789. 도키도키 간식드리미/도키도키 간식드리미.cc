087b7e791c37f7a4d1817c7e9b832a234acc7bfb
김예현 <77717564+KimYehyun@users.noreply.github.com>
2024-03-25 16:28:46 +0900

    [Silver III] Title: 도키도키 간식드리미, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/12789. 도키도키 간식드리미/도키도키 간식드리미.cc
+++ 백준/Silver/12789. 도키도키 간식드리미/도키도키 간식드리미.cc
@@ -1,51 +1,22 @@
-#include<iostream>
-#include<stack>
-#include<queue>
+#include <bits/stdc++.h>
 using namespace std;
-
-int n;
-queue<int>q;
+int n, t, cnt = 1;
 stack<int>s;
-
 int main(void) {
-
-	scanf("%d", &n);
-	for (int i = 0; i < n; i++) {
-		int a;
-		cin >> a;
-		q.push(a);
+	ios_base::sync_with_stdio(false);
+	cin.tie(0); cout.tie(0);
+    cin >> n;
+    while(n--){
+		cin >> t;
+		if(t==cnt) cnt++; 
+		else s.push(t); 
+		while(!s.empty() && s.top()==cnt){
+			s.pop(); 
+			cnt++;
+		}
 	}
 	
-	int key = 1;
-
-	while (1) {
-		bool a = 0;
-		bool b = 0;
-		bool c = 0;
-		if (!q.empty() && q.front() == key) {
-			q.pop();
-			key++;
-			a = 1;
-		}
-		else if (!s.empty() && s.top() == key) {
-			s.pop();
-			key++;
-			b = 1;
-		}
-		else if(!q.empty()){
-			s.push(q.front());
-			q.pop();
-			c = 1;
-		}
-	
-		if (q.empty() && s.empty()) {
-			cout << "Nice" << '\n';
-			return 0;
-		}
-		else if (a == 0 && b == 0 && c == 0) {
-			cout << "Sad" << '\n';
-			return 0;
-		}
-		
-	}
+	if(s.empty()) cout << "Nice"; 
+	else cout << "Sad";
+    return 0;
 }
