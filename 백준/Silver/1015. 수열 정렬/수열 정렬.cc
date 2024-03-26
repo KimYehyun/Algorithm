7ae2d8907ca52d48680855318184b23372b6bc69
김예현 <77717564+KimYehyun@users.noreply.github.com>
2024-03-26 20:41:02 +0900

    [Silver IV] Title: 수열 정렬, Time: 0 ms, Memory: 2032 KB -BaekjoonHub

---
+++ 백준/Silver/1015. 수열 정렬/수열 정렬.cc
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+int n;
+int a[1001],b[1001],ck[1001];
+vector <int> answer;
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(0); cout.tie(0);
+    cin >> n;
+    for(int i = 0; i < n; i++) {
+        cin >> a[i];
+        b[i] = a[i];
+    }
+    sort(b,b + n);
+    for(int i = 0; i < n; i++){
+        for(int j = 0; j < n; j++){
+            if(a[i] == b[j] && !ck[j]){
+                ck[j] = 1;
+                answer.push_back(j);
+                break;
+            }
+        }
+    }
+    for(auto p : answer) cout << p << " ";
+    return 0;
+}
