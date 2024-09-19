827f21027cf057e6a6c524a6fe24e5ae1a246bd9
김예현 <77717564+KimYehyun@users.noreply.github.com>
2024-09-19 15:07:17 +0900

    [Silver III] Title: 합 구하기, Time: 36 ms, Memory: 2800 KB -BaekjoonHub

--- 백준/Silver/11441. 합 구하기/합 구하기.cc
+++ 백준/Silver/11441. 합 구하기/합 구하기.cc
@@ -1,24 +1,26 @@
-#include<iostream>
+#include<bits/stdc++.h>
 using namespace std;
-int n, m, i, j;
-long long arr[1000001];
-int main(void) {
-	ios_base::sync_with_stdio(false);
-	cin.tie(0); cout.tie(0);
-	cin >> n;
+typedef long long ll;
+ll add[100001];
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(0); cout.tie(0);
 
-	cin >> arr[1];
-	for (int a = 2; a <= n; a++) {
-		cin >> arr[a];
-		arr[a] = arr[a] + arr[a - 1];
-	}
-	cin >> m;
-	for (int b = 0; b < m; b++) {
-		cin >> i >> j;
-		if (i == 1) cout << arr[j]<<"\n";
-		else {
-			cout << arr[j] - arr[i - 1]<<"\n";
-		}
-	}
-	return 0;
+    int n;
+    cin >> n;
+    for(int i=1;i<=n;i++){
+        int a;
+        cin >> a;
+        add[i] = add[i-1]+a;
+    }
+
+    int m;
+    cin >> m;
+    for(int i=0;i<m;i++){
+        int a,b;
+        cin >> a >> b;
+        cout << add[b] - add[a-1] <<"\n";   
+    }
+    
+    return 0;
 }
